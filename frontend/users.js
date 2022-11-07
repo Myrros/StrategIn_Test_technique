b415917737643a20045c4eee52dc056d3ae1e984
Valentin Lefèvre <56657464+Myrros@users.noreply.github.com>
2022-11-07 17:23:16 +0100

    Update users.js

--- frontend/users.js
+++ frontend/users.js
@@ -1,16 +1,23 @@
+// This function is called at the beginning and contains the event listeners.
+// It also verifies whether the user is logged or not.
 async function start() {
-    // We create an event listener on the 'send' button.
+
+    // We try and call the 'isLogged' function, which sends a request to the "islogged" endpoint, that is used to verify whether the user is connected or not.
     try {
-        isLogged = await isLogged(localStorage.getItem("token"));
+        await isLogged(localStorage.getItem("token"));
     } catch {
         console.log("You are not connected.");
+
+        // If the user is not connected, we display the html part for not connected users.
         document.getElementById("part_not_log").style.display = "block";
         return;
     }
 
+    // If the user is connected, we fetch the list of users.
     let users = await getUsers(localStorage.getItem("token"));
     let tableId = document.getElementById('users_table');
 
+    // We fill the table with the users fetched.
     for (let user of users) {
         let newLine = document.createElement('tr');
         for (n of ["email", "_id"]) {
@@ -21,13 +28,18 @@ async function start() {
         }
         tableId.appendChild(newLine);
     }
+
+    // We display the html part for connected users (the table of users).
     document.getElementById("part_log").style.display = "block";
+
+    // If the user wants to log out, we clear the local storage and refresh the page.
     document.getElementById('logout_button').onclick = () => {
         window.localStorage.clear();
         document.location.reload()
     };
 }
 
+// This function fetches the './users' endpoint, that requires authentification and returns the list of all user's mail and ID.
 async function getUsers(token) {
     let users;
     await axios({
@@ -43,6 +55,7 @@ async function getUsers(token) {
     return users
 };
 
+// This function checks whether the user is logged in or not.
 async function isLogged(token) {
     let islogged;
     await axios({
@@ -58,4 +71,5 @@ async function isLogged(token) {
     return islogged
 }
 
+// We can call the 'start' function.
 start();
