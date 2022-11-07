a02b52c049ce644fb4fe7fd611ad94af463ac508
Valentin Lefèvre <56657464+Myrros@users.noreply.github.com>
2022-11-07 17:22:21 +0100

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,15 +1,18 @@
-// This function is called at the beginning and contains all  event listeners.
+// This function is called at the beginning and contains the event listeners.
+// It also verifies whether the user is logged or not.
 async function start() {
+
+    // We try and call the 'isLogged' function, which sends a request to the "islogged" endpoint, that is used to verify whether the user is connected or not.
     try {
-        isLogged = await isLogged(localStorage.getItem("token"));
+        await isLogged(localStorage.getItem("token"));
     } catch {
         console.log("You are not connected.");
+
+        // If the user is not connected, we display the html part for not connected users and we prepare the login button.
         document.getElementById("part_not_log").style.display = "block";
         document.getElementById('login_button').onclick = async () => {
             const email = document.getElementById('email_input').value;
             const password = document.getElementById('password_input').value;
-    
-            // We send the event 'message' with the text entered by the user in data.
             await login(email, password);
     
             // Here, we reset the input placeholder.
@@ -20,15 +23,17 @@ async function start() {
         return;
     }
 
-    // We create an event listener on the 'send' button.
+    // If the user is connected, we display the html part for connected users (a link to the 'users' page and a log out button).
     document.getElementById("part_log").style.display = "block";
 
+    // If the user wants to log out, we clear the local storage and refresh the page.
     document.getElementById('logout_button').onclick = () => {
         window.localStorage.clear();
         document.location.reload()
     };
 }
 
+// This function logs the user in. If the information entered are correct, the token is stored in the local storage so that it can be used by other pages.
 async function login(email, password) {
     try {
         await axios({
@@ -47,7 +52,7 @@ async function login(email, password) {
     };
 };
 
-
+// This function checks whether the user is logged in or not.
 async function isLogged(token) {
     let islogged;
     await axios({
@@ -63,5 +68,5 @@ async function isLogged(token) {
     return islogged
 }
 
-// We call the start function in order to start listening to all events from the page.
+// We can call the 'start' function.
 start();
